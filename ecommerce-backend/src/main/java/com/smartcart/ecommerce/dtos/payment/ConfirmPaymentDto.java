1b8c22f76b2ece305861abf9387d9588a2db69b9
Shreyash Gurav <guravshreyash2005@gmail.com>
2026-02-12 23:50:10 +0530

    completed the auth dto and added the global reponse handler also created the error and response dtos

--- ecommerce-backend/src/main/java/com/smartcart/ecommerce/dtos/payment/ConfirmPaymentDto.java
+++ ecommerce-backend/src/main/java/com/smartcart/ecommerce/dtos/payment/ConfirmPaymentDto.java
@@ -1,4 +1,11 @@
 package com.smartcart.ecommerce.dtos.payment;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class ConfirmPaymentDto {
 }
--- ecommerce-backend/src/main/java/com/smartcart/ecommerce/dtos/user/UpdateUserProfileDto.java
+++ ecommerce-backend/src/main/java/com/smartcart/ecommerce/dtos/user/UpdateUserProfileDto.java
@@ -1,4 +1,11 @@
 package com.smartcart.ecommerce.dtos.user;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class UpdateUserProfileDto {
 }
